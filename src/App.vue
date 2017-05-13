e94f5cbc5814065ef0bfba8925bfad1724c03dad
Jakub Lenart <jakub@lenart.tech>
2017-05-13 10:10:27 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  #app
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="sass" rel="stylesheet/sass">
+
+  @import './assets/sass/main'
+
+</style>
