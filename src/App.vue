e49f5287a4de3b223229ed852f50ad55f83dc05c
Jakub Lenart <jakub@lenart.tech>
2017-05-13 13:35:03 +0200

    Add delivery form component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template lang="pug">
   #app
     main-header
+    .container.clearfix
+      h2.page-title {{ msg }}
+      .col.col-left
+        delivery-form
+
 </template>
 
 <script>
   import MainHeader from './components/Header.vue'
+  import DeliveryForm from './components/DeliveryForm.vue'
 
   export default {
     name: 'app',
+    data () {
+      return {
+        msg: 'Checkout'
+      }
+    },
     components: {
-      MainHeader
+      MainHeader,
+      DeliveryForm
     }
   }
 </script>
@@ -17,5 +29,13 @@
 <style lang="sass" rel="stylesheet/sass">
 
   @import './assets/sass/main'
+
+  .page-title
+    text-align: center
+    text-transform: uppercase
+    +font-px-to-rem(13, 15)
+    font-weight: $fw-bold
+    margin: 25px 0 40px
+    letter-spacing: 0.8px
 
 </style>
---
+++ src/components/DeliveryForm.vue
@@ -0,0 +1,71 @@
+<template lang="pug">
+    form
+      h3 Delivery Address
+      .field.req
+        label(for='firstname') First Name
+        input(type='text' name='firstname' id='firstname' required)
+      .field.req
+        label(for='lastname') Last Name
+        input(type='text' name='lastname' id='lastname' required)
+      .field.req
+        label(for='email') E-mail
+        input(type='email' name='email' id='email' required)
+      .field
+        label(for='phone') Phone
+        input(type='text' name='phone' id='phone')
+      .field.req
+        label(for='address') Address
+        input(type='text' name='address' id='address' required)
+      .field
+        label(for='address-cont') Address Cont.
+        input(type='text' name='address-cont' id='address-cont')
+      .field.req
+        label(for='zip-code') Zip Code
+        input(type='text' name='zip-code' id='zip-code' required)
+      .field.req
+        label(for='city') City
+        input(type='text' name='city' id='city' required)
+      .field.req
+        label(for='state') State
+        input(type='text' name='state' id='state' required)
+      .field.req
+        label(for='country') Country
+        .select-wrapper
+          select(name='country' id='country' required)
+            option
+            option United States
+      h3 Payment
+      .field.req
+        label(for='card-number') Card Number
+        input(type='text' name='card-number' id='card-number' required)
+      .field.req
+        label(for='card-holder') Card Holder
+        input(type='text' name='card-holder' id='card-holder' required)
+      .field.req
+        label(for='cvc') Cvc
+        input(type='text' name='cvc' id='cvc' required)
+
+      button(type='submit') Buy
+
+
+</template>
+
+<script>
+  export default {
+    name: 'delivery-form'
+  }
+</script>
+
+<style lang="sass" rel="stylesheet/sass" scoped>
+
+  @import '../assets/sass/mixins'
+
+  h3
+    padding-left: 12px
+    color: $font-color-alt
+
+  button
+    width: 100%
+
+
+</style>
