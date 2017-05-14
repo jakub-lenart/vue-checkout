b3fa1827dddf8d256223aad55fdb68e7bd683c97
Jakub Lenart <jakub@lenart.tech>
2017-05-14 16:47:56 +0200

    Add basic form validation

--- src/components/DeliveryForm.vue
+++ src/components/DeliveryForm.vue
@@ -1,58 +1,104 @@
 <template lang="pug">
-    form
+    form(@submit.prevent='sendForm' @change='validateForm')
+
       h3 Delivery Address
+
       .field.req
         label(for='firstname') First Name
-        input(type='text' name='firstname' id='firstname' required)
+        input(type='text' name='firstname' id='firstname' v-model='form.firstname' required)
+
       .field.req
         label(for='lastname') Last Name
-        input(type='text' name='lastname' id='lastname' required)
+        input(type='text' name='lastname' id='lastname' v-model='form.lastname' required)
+
       .field.req
         label(for='email') E-mail
-        input(type='email' name='email' id='email' required)
+        input(type='email' id='email' v-model='form.email' required)
+
       .field
         label(for='phone') Phone
         input(type='text' name='phone' id='phone')
+
       .field.req
         label(for='address') Address
-        input(type='text' name='address' id='address' required)
+        input(type='text' name='address' id='address' v-model='form.address' required)
+
       .field
         label(for='address-cont') Address Cont.
         input(type='text' name='address-cont' id='address-cont')
+
       .field.req
         label(for='zip-code') Zip Code
-        input(type='text' name='zip-code' id='zip-code' required)
+        input(type='text' name='zip-code' id='zip-code' v-model='form.zipCode' required)
+
       .field.req
         label(for='city') City
-        input(type='text' name='city' id='city' required)
+        input(type='text' name='city' id='city' v-model='form.city' required)
+
       .field.req
         label(for='state') State
-        input(type='text' name='state' id='state' required)
+        input(type='text' name='state' id='state' v-model='form.state' required)
+
       .field.req
         label(for='country') Country
         .select-wrapper
-          select(name='country' id='country' required)
+          select(name='country' id='country' v-model='form.country' required)
             option
             option United States
+
       h3 Payment
+
       .field.req
         label(for='card-number') Card Number
-        input(type='text' name='card-number' id='card-number' required)
+        input(type='text' name='card-number' v-model='form.cardNumber' id='card-number' required)
+
       .field.req
         label(for='card-holder') Card Holder
-        input(type='text' name='card-holder' id='card-holder' required)
+        input(type='text' name='card-holder' id='card-holder' v-model='form.cardHolder' required)
+
       .field.req
         label(for='cvc') Cvc
-        input(type='text' name='cvc' id='cvc' required)
+        input(type='text' name='cvc' id='cvc' v-model='form.cvc' required)
 
-      button(type='submit') Buy
+      button(type='submit' :disabled='!isFormValid') Buy
 
 
 </template>
 
 <script>
   export default {
-    name: 'delivery-form'
+    name: 'delivery-form',
+    data () {
+      return {
+        isFormValid: false,
+        // Only required fields
+        form: {
+          firstname: '',
+          lastname: '',
+          email: '',
+          address: '',
+          zipCode: '',
+          city: '',
+          state: '',
+          country: '',
+          cardNumber: '',
+          cardHolder: '',
+          cvc: ''
+        }
+      }
+    },
+    methods: {
+      sendForm: function () {
+        alert('form sent')
+      },
+      validateForm: function () {
+        let emptyFields = []
+        Object.values(this.form).map(item => {
+          if (item.length === 0) emptyFields.push(item)
+        })
+        this.isFormValid = emptyFields.length === 0;
+      }
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import VeeValidate from 'vee-validate';
+
+Vue.use(VeeValidate)
 
 new Vue({
   el: '#app',
